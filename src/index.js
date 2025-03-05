2869832a2573946160eac4a7ae939253c704d57b
frandipradana95 <frandipradana95@gmail.com>
2025-03-05 16:52:22 +0700

    merubah global style ke file js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import App from "./pages/app";
 import ReactDOM from "react-dom/client";
-import "./style.css";
+import { createGlobalStyle } from "styled-components";
 
+// global style
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Inter",sans-serif;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+
+
+a {
+  text-decoration: none;
+}`;
+
+// initial reactjs dan global style
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
+		<Global />
 		<App />
 	</React.StrictMode>
 );
